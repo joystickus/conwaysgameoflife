5f1bef865d48ee44b22e61389861bad1c0a91d7a
joystickus <joystickus@yandex.ru>
2020-06-22 22:35:59 +0300

    123

--- game of life v2.py
+++ game of life v2.py
@@ -9,7 +9,8 @@ baseCol,baseRow = 20, 15
 # Create a matrix.
 multiplier = 0 # This number means the scale of the matrix: 0,1,2,3 = 20x15,40x30,80x60,160x120.
                # It will be changed by the user and depending on the visibility of the pattern.
-col,row = baseCol*(2**multiplier),baseRow*(2**multiplier)
+# col,row = baseCol*(2**multiplier),baseRow*(2**multiplier)
+col,row = 10,10
 table = [[0 for y in range(col)] for x in range(row)]
 
 # Open a white board window based on the matrix.
@@ -26,11 +27,12 @@ pygame.display.flip()
 dot1 = [1, 1]
 dot2 = [1, 2]
 dot3 = [1, 3]
-dot4 = [1, 4]
-dot5 = [1, 5]
-pattern = [dot1, dot2, dot3, dot4, dot5]
-for i in range(len(pattern)):
-    table [pattern[i][0]] [pattern[i][1]] = 1
+# dot4 = [1, 4]
+# dot5 = [1, 5]
+# pattern = [dot1, dot2, dot3, dot4, dot5]
+pattern = [dot1, dot2, dot3]
+for dot in pattern:
+    table [dot[0]] [dot[1]] = 1
 
 # Show the pattern in the field.
 for i in range(row):
@@ -44,9 +46,9 @@ def calcEdges():
     rightEdge = 0
     patternColumns = []
     patternRows = []
-    for i in range(len(pattern)):
-        patternRows.append(pattern[i][0])
-        patternColumns.append(pattern[i][1])
+    for i in pattern:
+        patternRows.append(i[0])
+        patternColumns.append(i[1])
         topEdge = min(patternRows)
         bottomEdge = max(patternRows)
         leftEdge = min(patternColumns)
@@ -66,7 +68,6 @@ def check(rowDotPlace,colDotPlace):
                   "bottomRight": table [rowDotPlace+1] [colDotPlace+1]}
     count = 0
     center = False
-    future = ""
     if table [rowDotPlace] [colDotPlace] == 1:
         center = True
     for value in neighbours.values():
@@ -82,9 +83,45 @@ def check(rowDotPlace,colDotPlace):
         future = "nothing"
     return future
 
-# Function to check the dots of the pattern for changes
-def checkPattern(rowDotPlace,colDotPlace):
+# Function to check a cell, v2.
+def check2(rowDotPlace,colDotPlace):
+    count = 0
+    center = False
+    if table [rowDotPlace] [colDotPlace] == 1:
+        center = True
+    for i in range(-1,1):
+        for j in range(-1,1):
+            if table [rowDotPlace + i] [colDotPlace + j] == 1:
+                count += 1
+            if table[rowDotPlace][colDotPlace] == 1:
+                count -= 1
+    if center and 2 <= count <= 3:
+        future = "live"
+    elif center and (count < 2 or count > 3):
+        future = "die"
+    elif not center and count == 3:
+        future = "born"
+    else:
+        future = "nothing"
+    return future
 
+# Function to check the dots of the pattern for changes
+def checkPattern():
+    born = []
+    die = []
+    for i in pattern:
+        for j in range(-1,1):
+            for k in range(-1,1):
+                result = check(i[0]+j, i[1]+k)
+                if result == "die":
+                    die.append(i)
+                elif result == "born":
+                    born.append(i)
+    print("born", born, ", die", die)
+    for i in born:
+        table[i[0]][i[1]] = 1
+    for i in die:
+        table[i[0]][i[1]] = 0
 
 # Function to check the whole matrix and change its state.
 def checkMatrix():
@@ -97,10 +134,10 @@ def checkMatrix():
                 born.append([i,j])
             elif result == "die":
                 die.append([i,j])
-    for i in range(len(born)):
-        table [born[i][0]] [born[i][1]] = 1
-    for i in range(len(die)):
-        table [die[i][0]] [die[i][1]] = 0
+    for i in born:
+        table [i[0]] [i[1]] = 1
+    for i in die:
+        table [i[0]] [i[1]] = 0
 
 # Center the pattern in the field.
 edges = calcEdges() # get the edges of the pattern
@@ -110,21 +147,21 @@ py = int((edges[0] + edges[1]) / 2) # calculate the center of the pattern
 px = int((edges[2] + edges[3]) / 2)
 moveY = my - py # calculate the distance to move the pattern to the center
 moveX = mx - px
-for i in range(len(pattern)): # move the pattern to the center
-    pattern[i][0] += moveY
-    pattern[i][1] += moveX
+for i in pattern: # move the pattern to the center
+    i[0] += moveY
+    i[1] += moveX
 # flush and redraw the field
 table = [[0 for y in range(col)] for x in range(row)]
-for i in range(len(pattern)):
-    table [pattern[i][0]] [pattern[i][1]] = 1
+for i in pattern:
+    table [i[0]] [i[1]] = 1
 
-print()
+print(pattern)
 for i in range(row):
     print(table[i])
 
 # Give it a try
-for i in range(10):
-    checkMatrix()
+for i in range(3):
+    checkPattern()
     time.sleep(0.3)
     print()
     for j in range(row):
