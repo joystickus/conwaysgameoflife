48bd8082b591f4e0e007191f374b8168f674956a
joystickus <joystickus@yandex.ru>
2020-06-23 18:55:44 +0300

    123

--- game of life v2.py
+++ game of life v2.py
@@ -11,6 +11,8 @@ multiplier = 0 # This number means the scale of the matrix: 0,1,2,3 = 20x15,40x3
                # It will be changed by the user and depending on the visibility of the pattern.
 col,row = baseCol*(2**multiplier),baseRow*(2**multiplier)
 table = [[0 for y in range(col)] for x in range(row)]
+cellSize = 800/col
+dotRadius = int(cellSize/2*0.8)
 
 # Open a white board window based on the matrix.
 screen = pygame.display.set_mode([800,600])
@@ -31,6 +33,9 @@ dot5 = [1, 5]
 pattern = [dot1, dot2, dot3, dot4, dot5]
 for dot in pattern:
     table [dot[0]] [dot[1]] = 1
+    dotPlace = [int(dot[1]*cellSize+cellSize/2), int(dot[0]*cellSize+cellSize/2)]
+    pygame.draw.circle(screen, [0,0,0], dotPlace, dotRadius, 0)
+pygame.display.flip()
 
 # Show the field with pattern in it.
 for i in range(row):
@@ -112,7 +117,7 @@ for i in range(row):
     print(table[i])
 
 # Give it a try
-for i in range(10):
+for i in range(2):
     time.sleep(0.3)
     move()
 
